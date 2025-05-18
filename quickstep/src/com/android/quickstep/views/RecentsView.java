294bc3ae03faa7aec728c759780f9a4133ce7176
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-05-18 10:45:21 +0530

    Revert "Launcher3: Disable live tile"
    This reverts commit a2528afc781bdead73ca15b8962ee7c785eb524d.

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -2797,10 +2797,6 @@ public abstract class RecentsView<CONTAINER_TYPE extends Context & RecentsViewCo
         animateActionsViewIn();
 
         mCurrentGestureEndTarget = null;
-
-        switchToScreenshot(
-            () -> finishRecentsAnimation(true /* toRecents */, false /* shouldPip */,
-                    null));
     }
 
     /**
